2c57f1e995948028daabeef921192fe8aad3f69f
abissonho <alexbissonho@gmail.com>
2023-03-18 11:40:46 -0300

    exercicio 14

---
+++ modulos/D/exercicios/ex014/script.js
@@ -0,0 +1,22 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('foto')
+    
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    
+    if (hora >= 0 && hora < 12){
+        //bom dia
+        img.src = './imagens/foto-manha.png'
+        document.body.style.background = '#617F93'
+    } else if( hora >= 12 && hora <= 18){
+        //boa tarde
+        img.src = './imagens/foto-tarde.png'
+        document.body.style.background = '#D94D1A   '
+    } else {
+        //boa noite
+        img.src = './imagens/foto-noite.png'
+        document.body.style.background = '#111426'
+    }
+}
