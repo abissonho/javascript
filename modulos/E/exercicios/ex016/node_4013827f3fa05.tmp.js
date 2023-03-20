210be0735e800dc736498d2dca85969dd7cc9fca
abissonho <alexbissonho@gmail.com>
2023-03-20 10:39:49 -0300

    ex016

---
+++ modulos/E/exercicios/ex016/node_4013827f3fa05.tmp.js
@@ -0,0 +1,19 @@
+var c = 1
+while (c < 6){
+    console.log(' Tudo Bem?')
+}
+
+
+
+
+
+
+
+
+
+/*console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')*/
