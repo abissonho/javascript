1698dc13d367ff01c855ee92c09005d1f66144ab
abissonho <alexbissonho@gmail.com>
2023-03-20 11:10:38 -0300

    ex016 e 017

--- modulos/E/exercicios/ex016/ambiente.js
+++ modulos/E/exercicios/ex016/ambiente.js
@@ -1,1 +1,16 @@
-console.log('tudo bem?')
+//Estrutura Simples De Repetição
+
+var c = 1
+/*while (c < 6){
+    console.log(`Passo ${c} T`)
+    c++
+} */
+
+/*do{
+    console.log(`Passo ${c}`)
+    c++
+} while(c <= 6) */
+
+for(var c = 1; c <= 10; c++){
+    console.log(`Passo ${c}`)
+}
